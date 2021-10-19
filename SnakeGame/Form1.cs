0eb51a50b709e0398a3d7ffb9bde1f1981ee5b4b
Miroslav Stejskal <Stejskal.Miroslav@stud.vassboskovice.cz>
2021-10-19 13:06:24 +0200

    generování hráču do pole

--- SnakeGame/Form1.cs
+++ SnakeGame/Form1.cs
@@ -12,9 +12,27 @@ namespace SnakeGame
 {
     public partial class Form1 : Form
     {
+        PictureBox snakeOne;
+        PictureBox snakeTwo;
+
         public Form1()
         {
             InitializeComponent();
+            CreateSnakes();
+        }
+        private void CreateSnakes()
+        {
+            snakeOne = new PictureBox();
+            snakeOne.Size = new Size(50,50);
+            snakeOne.Location = new Point(10,10);
+            snakeOne.BackColor = Color.Red;
+            Controls.Add(snakeOne);
+
+            snakeTwo = new PictureBox();
+            snakeTwo.Size = new Size(50, 50);
+            snakeTwo.Location = new Point(ClientSize.Width-60, 10);
+            snakeTwo.BackColor = Color.Blue;
+            Controls.Add(snakeTwo);
         }
 
     }
