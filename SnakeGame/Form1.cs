fb2772b435042bcd379929f8c2429b7975611880
Mira Stejskal <65676574+swajp@users.noreply.github.com>
2021-10-20 15:06:00 +0200

    Update Form1.cs

--- SnakeGame/Form1.cs
+++ SnakeGame/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,10 +13,14 @@ namespace SnakeGame
     public partial class Form1 : Form
     {
         PictureBox snakeOne;
-        List<PictureBox> tailSnake;
-        PictureBox snakeTwo;
+        List<PictureBox> tailSnake1;
         double direction1;
         double speed1;
+
+        PictureBox snakeTwo;
+        List<PictureBox> tailSnake2;
+        double direction2;
+        double speed2;
 
         Timer timer;
 
@@ -25,6 +29,7 @@ namespace SnakeGame
             InitializeComponent();
             this.KeyPreview = true;
             this.KeyDown += Form1_KeyDown;
+            this.KeyDown += Form2_KeyDown;
             CreateSnakes();
             timer = new Timer();
             timer.Interval = 50;
@@ -42,22 +47,42 @@ namespace SnakeGame
                 direction1 -= 0.1;
             }
         }
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                direction2 += 0.1;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                direction2 -= 0.1;
+            }
+        }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            tailSnake.Add(snakeOne);
+            tailSnake1.Add(snakeOne);
             snakeOne = new PictureBox();
             snakeOne.Size = new Size(13, 13);
             var changeX = speed1 * Math.Cos(direction1);
             var changeY = speed1 * -Math.Sin(direction1);
-            snakeOne.Location = new Point(tailSnake.Last().Location.X + (int)changeX, tailSnake.Last().Location.Y + (int)changeY);
+            snakeOne.Location = new Point(tailSnake1.Last().Location.X + (int)changeX, tailSnake1.Last().Location.Y + (int)changeY);
             snakeOne.BackColor = Color.Red;
             Controls.Add(snakeOne);
+
+            tailSnake2.Add(snakeTwo);
+            snakeTwo = new PictureBox();
+            snakeTwo.Size = new Size(13, 13);
+            var changeX2 = speed2 * Math.Cos(direction2);
+            var changeY2 = speed2 * -Math.Sin(direction2);
+            snakeTwo.Location = new Point(tailSnake2.Last().Location.X + (int)changeX2, tailSnake2.Last().Location.Y + (int)changeY2);
+            snakeTwo.BackColor = Color.Blue;
+            Controls.Add(snakeTwo);
         }
 
         private void CreateSnakes()
         {
-            tailSnake = new List<PictureBox>();
+            tailSnake1 = new List<PictureBox>();
             direction1 = 0;
             speed1 = 2;
             snakeOne = new PictureBox();
@@ -65,10 +90,13 @@ namespace SnakeGame
             snakeOne.Location = new Point(10,10);
             snakeOne.BackColor = Color.Red;
             Controls.Add(snakeOne);
-            
+
+            tailSnake2 = new List<PictureBox>();
+            direction1 = 0;
+            speed2 = 2;
             snakeTwo = new PictureBox();
             snakeTwo.Size = new Size(13, 13);
-            snakeTwo.Location = new Point(ClientSize.Width-60, 10);
+            snakeTwo.Location = new Point(ClientSize.Width - 60, 10);
             snakeTwo.BackColor = Color.Blue;
             Controls.Add(snakeTwo);
         }
